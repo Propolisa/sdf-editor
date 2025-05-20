53058bec0178a4a9c741524b1eb7d9e8c4aa4994
Chris Stanley <(none)>
2025-05-20 02:33:00 +0200

    Updates

---
+++ spa/assets/window--JWE_rE9.js
@@ -0,0 +1,1 @@
+import{d as i}from"./index-CYenuLRQ.js";const e=i(({app:o})=>{o.config.globalProperties.window=window});export{e as default};
--- spa/assets/window-DAvJjqc0.js
+++
@@ -1,1 +0,0 @@
-import{d as i}from"./index-rLiCEjyZ.js";const e=i(({app:o})=>{o.config.globalProperties.window=window});export{e as default};
