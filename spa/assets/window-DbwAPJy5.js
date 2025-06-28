75fc9c4cbffa02f3c8c185aa82977c3211972268
Chris Stanley <chris.stanley@internal>
2025-06-28 13:02:10 +0200

    Updates

--- spa/assets/window-DbwAPJy5.js
+++
@@ -1,1 +0,0 @@
-import{d as i}from"./index-DDXWBF54.js";const e=i(({app:o})=>{o.config.globalProperties.window=window});export{e as default};
---
+++ spa/assets/window-Mj774rjs.js
@@ -0,0 +1,1 @@
+import{d as i}from"./index-Bic393x9.js";const e=i(({app:o})=>{o.config.globalProperties.window=window});export{e as default};
